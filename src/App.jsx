7659a9b918cdb27f16cb02bd993d46d5db9278cc
Vanderlei Chagas <vanderleichagas42@gmail.com>
2023-05-10 20:50:48 -0300

    wip: implement midime training

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import { MidiMe } from "@magenta/music";
 const player = new mm.Player();
 const model = new mm.MusicVAE(
-  "https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_16bar_small_q2"
+  "https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_2bar_small"
 );
-
+const MELODY_BARS = 2;
 async function generateMusicVaeMelody() {
   let musicVaeMelody;
 
@@ -28,6 +28,57 @@ async function generateMusicVaeMelody() {
 
   await generate();
   download();
+}
+
+async function generateMidiMeMelody() {
+  const training = {};
+  await mm.Player.tone.context.resume(); // enable audio
+
+  function loadFiles() {
+    const fileInput = document.getElementById("fileInput");
+
+    const promises = [];
+    for (let i = 0; i < fileInput.files.length; i++) {
+      promises.push(mm.blobToNoteSequence(fileInput.files[i]));
+    }
+    return Promise.all(promises).then((res) => res);
+  }
+
+  function getChunks(quantizedMels) {
+    let chunks = [];
+    quantizedMels.forEach((m) => {
+      const melChunks = mm.sequences.split(
+        mm.sequences.clone(m),
+        16 * MELODY_BARS
+      );
+      chunks = chunks.concat(melChunks);
+    });
+    return chunks;
+  }
+
+  const noteSequences = await loadFiles();
+  const quantizedSequence = mm.sequences.quantizeNoteSequence(
+    noteSequences[0],
+    4
+  );
+
+  const chunks = getChunks([quantizedSequence]);
+  console.log(chunks);
+  const z = await model.encode(chunks);
+
+  const midime = new MidiMe({ epochs: 100 });
+  midime.initialize();
+
+  await midime.train(z, async (epoch, logs) => {
+    console.log("epoch + 1", epoch + 1);
+    console.log("plot loss", logs.total);
+  });
+
+  const s = await midime.sample(1);
+  const zArray = s.arraySync()[0];
+  const currentSample = (await model.decode(s))[0];
+  player.start(currentSample);
+  console.log(currentSample);
 }
 
 function App() {
@@ -64,11 +115,12 @@ function App() {
           <option>Martin Garrix</option>
         </select>
       </div>
-
+      <input type="file" id="fileInput" multiple />
       <button
         className="btn btn-success mt-3"
         dowload="melody"
-        onClick={generateMusicVaeMelody}
+        onClick={generateMidiMeMelody}
+        // onClick={generateMusicVaeMelody}
         // onClick={initializeMidiMe}
         //onClick={generateMidi}
       >
