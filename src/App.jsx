4bbcc2d9c7d5347c741b06f58feffb40856791e3
Vanderlei Chagas <vanderleichagas42@gmail.com>
2023-05-11 18:13:59 -0300

    feat: fetch midi files from server

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,34 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { MidiMe } from "@magenta/music";
-const player = new mm.Player();
+import { useState } from "react";
+
 const model = new mm.MusicVAE(
   "https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_2bar_small"
 );
 const MELODY_BARS = 2;
+
+const serverUrl = "http://192.168.0.4:8000";
+
 async function generateMusicVaeMelody() {
   let musicVaeMelody;
 
   async function generate() {
     await mm.Player.tone.context.resume(); // enable audio
-    await model.sample(1).then((samples) => {
-      musicVaeMelody = samples[0]; // store musicVaeMelody for download
-      player.start(musicVaeMelody);
+    await model.initialize().then(async () => {
+      await model.sample(1).then((samples) => {
+        musicVaeMelody = samples[0];
+        //player.start(musicVaeMelody);
+      });
     });
   }
-
+  console.log(musicVaeMelody);
   function download() {
     if (!musicVaeMelody) {
       alert("You must generate a musicVaeMelody before you can download it!");
     } else {
-      saveAs(new File([mm.sequenceProtoToMidi(musicVaeMelody)], "trio.mid"));
+      musicVaeMelody.notes.forEach((note) => (note.velocity = 100));
+      saveAs(new File([mm.sequenceProtoToMidi(musicVaeMelody)], "melody.mid"));
     }
   }
 
@@ -30,58 +37,77 @@ async function generateMusicVaeMelody() {
   download();
 }
 
-async function generateMidiMeMelody() {
-  const training = {};
-  await mm.Player.tone.context.resume(); // enable audio
-
-  function loadFiles() {
-    const fileInput = document.getElementById("fileInput");
-
-    const promises = [];
-    for (let i = 0; i < fileInput.files.length; i++) {
-      promises.push(mm.blobToNoteSequence(fileInput.files[i]));
-    }
-    return Promise.all(promises).then((res) => res);
-  }
-
-  function getChunks(quantizedMels) {
-    let chunks = [];
-    quantizedMels.forEach((m) => {
-      const melChunks = mm.sequences.split(
-        mm.sequences.clone(m),
-        16 * MELODY_BARS
-      );
-      chunks = chunks.concat(melChunks);
-    });
-    return chunks;
-  }
-
-  const noteSequences = await loadFiles();
-  const quantizedSequence = mm.sequences.quantizeNoteSequence(
-    noteSequences[0],
-    4
-  );
-
-  const chunks = getChunks([quantizedSequence]);
-  console.log(chunks);
-  const z = await model.encode(chunks);
-
-  const midime = new MidiMe({ epochs: 100 });
-  midime.initialize();
-
-  await midime.train(z, async (epoch, logs) => {
-    console.log("epoch + 1", epoch + 1);
-    console.log("plot loss", logs.total);
-  });
-
-  const s = await midime.sample(1);
-  const zArray = s.arraySync()[0];
-  const currentSample = (await model.decode(s))[0];
-  player.start(currentSample);
-  console.log(currentSample);
-}
+const fetchFileNames = async (artistName) => {
+  return await fetch(`${serverUrl}/midi-files/${artistName}`)
+    .then((res) => res.json())
+    .then((res) => res.files);
+};
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  async function generateMidiMeMelody() {
+    let midiMeMelody;
+    const artistName = "martin-garrix";
+
+    setLoading(true);
+
+    function getChunks(quantizedMels) {
+      let chunks = [];
+      quantizedMels.forEach((m) => {
+        const melChunks = mm.sequences.split(
+          mm.sequences.clone(m),
+          16 * MELODY_BARS
+        );
+        chunks = chunks.concat(melChunks);
+      });
+      return chunks;
+    }
+
+    function loadMidi(fileNames, artistName) {
+      const promises = [];
+
+      fileNames.forEach((fileName) => {
+        const fileUrl = `${serverUrl}/midi-files/${artistName}/${fileName}`;
+        const promise = mm.urlToBlob(fileUrl).then(async (blob) => {
+          return await mm.blobToNoteSequence(blob);
+        });
+        promises.push(promise);
+      });
+
+      return Promise.all(promises).then((res) => res);
+    }
+
+    const fileNames = await fetchFileNames(artistName);
+
+    const midisequencesFromServer = await loadMidi(fileNames, artistName);
+
+    const quantizedSequences = midisequencesFromServer.map((sequence) =>
+      mm.sequences.quantizeNoteSequence(sequence, 4)
+    );
+
+    const chunks = getChunks(quantizedSequences);
+    const z = await model.encode(chunks);
+
+    const midime = new MidiMe({ epochs: 100 });
+    midime.initialize();
+
+    await midime.train(z, async (epoch, logs) => {
+      console.log("epoch + 1", epoch + 1);
+      console.log("plot loss", logs.total);
+    });
+
+    const s = await midime.sample(1);
+    const zArray = s.arraySync()[0];
+    midiMeMelody = (await model.decode(s))[0];
+
+    if (!midiMeMelody) return;
+    midiMeMelody.notes.forEach((note) => (note.velocity = 100));
+    saveAs(new File([mm.sequenceProtoToMidi(midiMeMelody)], "melody.mid"));
+
+    setLoading(false);
+  }
+
   return (
     <div className="App">
       <div>
@@ -93,19 +119,6 @@ function App() {
         </a>
       </div>
       <h1>MELO AI</h1>
-      <div className="mt-3">
-        <select name="key" id="key">
-          <option>Em</option>
-          <option>Fm</option>
-        </select>
-      </div>
-
-      <div className="mt-3">
-        <select name="amount-of-bars" id="amount-of-bars">
-          <option>8</option>
-          <option>16</option>
-        </select>
-      </div>
 
       <div className="mt-3">
         <select name="artist" id="artist">
@@ -115,16 +128,17 @@ function App() {
           <option>Martin Garrix</option>
         </select>
       </div>
-      <input type="file" id="fileInput" multiple />
+
       <button
         className="btn btn-success mt-3"
         dowload="melody"
         onClick={generateMidiMeMelody}
-        // onClick={generateMusicVaeMelody}
+        type="button"
+        //onClick={generateMusicVaeMelody}
         // onClick={initializeMidiMe}
         //onClick={generateMidi}
       >
-        Generate
+        {!loading ? "Generate" : "..."}
       </button>
     </div>
   );
